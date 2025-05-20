7946ef7f33ef1cf16d0f0a79e4e92391a5e48906
Ciprian Goea <florin.c.goea@intel.com>
2025-05-20 15:09:20 +0300

    fail on error

--- common.mk
+++ common.mk
@@ -74,10 +74,10 @@ help:
 	@printf "%-20s %s\n" "Target" "Description"
 	@printf "%-20s %s\n" "------" "-----------"
 	@make -pqR : 2>/dev/null \
-	| awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' \
-	| sort \
-	| egrep -v -e '^[^[:alnum:]]' -e '^$@$$' \
-	| xargs -I _ sh -c 'printf "%-20s " _; grep -B 1 "^_" Makefile | (grep -i "^# Help:" || echo "") | tail -1 | sed "s/^# Help: //g"'
+    | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' \
+    | sort \
+    | egrep -v -e '^[^[:alnum:]]' -e '^$@$$' \
+    | xargs -I _ sh -c 'printf "%-20s " _; grep -B 1 "^_" Makefile | (grep -i "^# Help:" || echo "") | tail -1 | sed "s/^# Help: //g"'
 
 #### Build debian packages ####
 
