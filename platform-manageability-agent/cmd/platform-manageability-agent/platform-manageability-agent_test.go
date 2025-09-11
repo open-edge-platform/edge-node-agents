8145d9d6c423f9ae25c0a0ff3eb7d4cb8318a260
cjnolan <christopher.nolan@intel.com>
2025-09-11 16:54:13 +0000

    Add DM Manager mock and integration tests for PMA

--- platform-manageability-agent/cmd/platform-manageability-agent/platform-manageability-agent_test.go
+++ platform-manageability-agent/cmd/platform-manageability-agent/platform-manageability-agent_test.go
@@ -1,18 +1,151 @@
 // SPDX-FileCopyrightText: (C) 2025 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-package main
+package main_test
 
-import "testing"
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"testing"
+	"time"
 
-// TODO: Add tests for platform-manageability-agent
-func TestMainRuns(t *testing.T) {
-	// This is a placeholder test to ensure main package compiles and runs
-	// Add more meaningful tests as functionality is implemented
-	defer func() {
-		if r := recover(); r != nil {
-			t.Errorf("main panicked: %v", r)
+	"github.com/stretchr/testify/require"
+)
+
+// Test is configured via environment files, paths to PMA and mock executables
+// PMA configuration file & mock certificate/key must be provided
+var platformManageabilityAgentBinary string = os.Getenv("PMA_BINARY_PATH")
+var dmManagerMockBinary string = os.Getenv("DMM_BINARY_PATH")
+var statusServerMockBinary string = os.Getenv("SS_BINARY_PATH")
+var testConfig string = os.Getenv("TEST_CONFIG_PATH")
+var testCertificate string = os.Getenv("TEST_CERT_PATH")
+var testPrivateKey string = os.Getenv("TEST_KEY_PATH")
+var pmaVersion string = os.Getenv("PMA_VERSION")
+
+// global timeout for entire test
+const testTimeout = 60 * time.Second
+
+var comListenAddress string = "localhost:12345"
+
+var pmaBuffer bytes.Buffer
+var mockBuffer bytes.Buffer
+
+// Execute Platform Manageability Agent smoke test
+// The test intent is to check if Platform Manageability Agent is able to detect AMT status from the node
+// and apply the required profile.
+// DM Manager mock is used to return static data to the PMA.
+// Test is successful if no errors are detected
+func TestDeviceConfiguration(t *testing.T) {
+	require.NoError(t, validateInputVars())
+
+	ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+	defer cancel()
+
+	fmt.Println("Starting DM Manager Mock...")
+	dmCmd, err := startMock(ctx, dmManagerMockBinary)
+	require.NoError(t, err)
+
+	fmt.Println("Starting Status Server Mock...")
+	ssmCmd, err := startStatusServerMock(ctx, statusServerMockBinary)
+	require.NoError(t, err)
+
+	fmt.Println("Starting Platform Manageability Agent...")
+	pmaCmd, err := startPlatformManageabilityAgent(ctx, platformManageabilityAgentBinary)
+	require.NoError(t, err)
+
+	time.Sleep(20 * time.Second)
+	require.Contains(t, pmaBuffer.String(), "Platform Manageability Agent started successfully")
+	require.Contains(t, pmaBuffer.String(), "Successfully reported AMT status for host")
+	require.Contains(t, pmaBuffer.String(), "Successfully retrieved activation details for host")
+	require.Contains(t, pmaBuffer.String(), "Status Ready")
+	require.Contains(t, mockBuffer.String(), "AMTStatusReport")
+	require.Contains(t, mockBuffer.String(), "ActivationRequest")
+	require.Contains(t, mockBuffer.String(), "ActivationResponseDetails")
+	require.Contains(t, mockBuffer.String(), "ActivationResultRequest")
+	pmaBuffer.Reset()
+	mockBuffer.Reset()
+
+	fmt.Println("Stopping Platform Manageability Agent...")
+	_ = killProcess(ctx, pmaCmd.Process.Pid)
+	err = pmaCmd.Wait()
+	require.NoError(t, err)
+
+	fmt.Println("Stopping Status Server Mock...")
+	_ = killProcess(ctx, ssmCmd.Process.Pid)
+
+	fmt.Println("Stopping DM Manager Mock...")
+	_ = killProcess(ctx, dmCmd.Process.Pid)
+	// FIXME: DM Manager Mock doesn't exit cleanly on SIGTERM
+	_ = dmCmd.Wait()
+
+	require.NoError(t, ctx.Err(), "Test took too long!")
+}
+
+// Execute `platform-manageability-agent version` and check if it returns proper version and exits with 0 return code
+func TestVersion(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+	defer cancel()
+
+	fmt.Printf("Checking PMA version; Expected: %v", pmaVersion)
+	out, err := exec.CommandContext(ctx, platformManageabilityAgentBinary, "version").Output()
+	require.NoError(t, err)
+
+	require.Equal(t, pmaVersion+"\n", string(out))
+}
+
+// Execute `platform-manageability-agent` without arguments. It should exit with 1 return code and priont usage message.
+func TestInvalidArguments(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+	defer cancel()
+
+	out, err := exec.CommandContext(ctx, platformManageabilityAgentBinary).CombinedOutput()
+	require.Error(t, err)
+	require.Contains(t, string(out), "Error: --config flag is required and must not be empty")
+
+	out, err = exec.CommandContext(ctx, platformManageabilityAgentBinary, "-config", "").CombinedOutput()
+	require.Error(t, err)
+	require.Contains(t, string(out), "Error: --config flag is required and must not be empty")
+
+	out, err = exec.CommandContext(ctx, platformManageabilityAgentBinary, "-config", "no/such/config").CombinedOutput()
+	require.Error(t, err)
+	require.Contains(t, string(out), "unable to initialize configuration")
+}
+
+func startMock(ctx context.Context, dmManagerMockBinary string) (*exec.Cmd, error) {
+	cmd := exec.CommandContext(ctx, dmManagerMockBinary,
+		"-certPath", testCertificate,
+		"-keyPath", testPrivateKey,
+		"-address", comListenAddress)
+	cmd.Stderr = &mockBuffer
+	return cmd, cmd.Start()
+}
+
+func startStatusServerMock(ctx context.Context, statusServerMockBinary string) (*exec.Cmd, error) {
+	cmd := exec.CommandContext(ctx, statusServerMockBinary)
+	return cmd, cmd.Start()
+}
+
+func startPlatformManageabilityAgent(ctx context.Context, platformManageabilityAgentBinary string) (*exec.Cmd, error) {
+	cmd := exec.CommandContext(ctx, platformManageabilityAgentBinary, "-config", testConfig)
+	cmd.Stdout = io.MultiWriter(&pmaBuffer, os.Stdout)
+	cmd.Stderr = os.Stderr
+	return cmd, cmd.Start()
+}
+
+func killProcess(ctx context.Context, pid int) error {
+	stop_cmd := exec.CommandContext(ctx, "kill", fmt.Sprintf("%v", pid))
+	return stop_cmd.Run()
+}
+
+func validateInputVars() error {
+	for _, p := range []string{platformManageabilityAgentBinary, dmManagerMockBinary, testConfig, testCertificate, testPrivateKey} {
+		if _, err := os.Stat(p); err != nil {
+			return err
 		}
-	}()
-	go main() // main() should not panic
+	}
+	return nil
 }
--- platform-manageability-agent/internal/utils/utils.go
+++ platform-manageability-agent/internal/utils/utils.go
@@ -5,7 +5,9 @@ package utils
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"slices"
 	"time"
 
 	log "github.com/open-edge-platform/edge-node-agents/platform-manageability-agent/internal/logger"
@@ -21,13 +23,35 @@ type CommandExecutor interface {
 type RealCommandExecutor struct{}
 
 func ExecuteCommand(command string, args []string) ([]byte, error) {
-	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
-	if err != nil {
-		log.Logger.Errorf("Failed to execute command %s with args %v: %v", command, args, err)
-		return nil, fmt.Errorf("failed to execute command %s with args %v: %w", command, args, err)
+	// Check if agent is being run with the DM manager mock and skip command if so
+	_, testCheck := os.LookupEnv("PMA_BINARY_PATH")
+	if !testCheck {
+		cmd := exec.Command(command, args...)
+		output, err := cmd.Output()
+		if err != nil {
+			log.Logger.Errorf("Failed to execute command %s with args %v: %v", command, args, err)
+			return nil, fmt.Errorf("failed to execute command %s with args %v: %w", command, args, err)
+		}
+		return output, nil
+	} else {
+		if slices.Contains(args, "is-active") {
+			cmd := exec.Command("echo", "active")
+			output, err := cmd.Output()
+			if err != nil {
+				log.Logger.Errorf("Failed to execute command echo with args active: %v", err)
+				return nil, fmt.Errorf("failed to execute command echo with args active: %v", err)
+			}
+			return output, nil
+		} else {
+			cmd := exec.Command("echo", "success")
+			output, err := cmd.Output()
+			if err != nil {
+				log.Logger.Errorf("failed to execute command echo with args success: %v", err)
+				return nil, fmt.Errorf("failed to execute command echo with args success: %v", err)
+			}
+			return output, nil
+		}
 	}
-	return output, nil
 }
 
 // ExecuteAMTInfo executes the AMT info command with retries.
@@ -38,12 +62,23 @@ func (r *RealCommandExecutor) ExecuteAMTInfo() ([]byte, error) {
 
 	var err error
 	for i := 1; i <= maxRetries; i++ {
-		cmd := exec.Command("sudo", "/usr/bin/rpc", "amtinfo")
-		output, err = cmd.CombinedOutput()
-		if err == nil {
-			return output, nil
+		// Check if agent is being run with the DM manager mock and skip command if so
+		_, testCheck := os.LookupEnv("PMA_BINARY_PATH")
+		if !testCheck {
+			cmd := exec.Command("sudo", "/usr/bin/rpc", "amtinfo")
+			output, err = cmd.CombinedOutput()
+			if err == nil {
+				return output, nil
+			}
+			log.Logger.Warnf("Failed to execute AMT info command (attempt %d/%d): %v", i, maxRetries, err)
+		} else {
+			cmd := exec.Command("printf", "Version: 0.1.0\nRAS Remote Status: not connected\n")
+			output, err = cmd.CombinedOutput()
+			if err == nil {
+				return output, nil
+			}
+			log.Logger.Warnf("Failed to execute printf command (attempt %d/%d): %v", i, maxRetries, err)
 		}
-		log.Logger.Warnf("Failed to execute AMT info command (attempt %d/%d): %v", i, maxRetries, err)
 		if i < maxRetries {
 			time.Sleep(retryInterval)
 		}
@@ -53,8 +88,15 @@ func (r *RealCommandExecutor) ExecuteAMTInfo() ([]byte, error) {
 
 // ExecuteAMTActivate executes the AMT activate command.
 func (r *RealCommandExecutor) ExecuteAMTActivate(rpsAddress, profileName, password string) ([]byte, error) {
-	cmd := exec.Command("sudo", "-E", "/usr/bin/rpc", "activate", "-u", rpsAddress, "-n")
-	cmd.Env = append(cmd.Environ(), fmt.Sprintf("AMT_PASSWORD=%s", password))
-	cmd.Env = append(cmd.Environ(), fmt.Sprintf("PROFILE=%s", profileName))
-	return cmd.CombinedOutput()
+	// Check if agent is being run with the DM manager mock and skip command if so
+	_, testCheck := os.LookupEnv("PMA_BINARY_PATH")
+	if !testCheck {
+		cmd := exec.Command("sudo", "-E", "/usr/bin/rpc", "activate", "-u", rpsAddress, "-n")
+		cmd.Env = append(cmd.Environ(), fmt.Sprintf("AMT_PASSWORD=%s", password))
+		cmd.Env = append(cmd.Environ(), fmt.Sprintf("PROFILE=%s", profileName))
+		return cmd.CombinedOutput()
+	} else {
+		cmd := exec.Command("echo", "success")
+		return cmd.CombinedOutput()
+	}
 }
