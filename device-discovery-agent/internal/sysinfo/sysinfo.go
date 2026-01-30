dbcd1f7e4872ac0c9907e0a73ceea63c0a777071
ppanigra <piyush.panigrahi@intel.com>
2026-01-30 13:58:42 +0530

    Update sysinfo package to require sudo for dmidecode commands

--- device-discovery-agent/internal/sysinfo/sysinfo.go
+++ device-discovery-agent/internal/sysinfo/sysinfo.go
@@ -15,7 +15,7 @@ import (
 
 // GetSerialNumber retrieves the serial number of the machine.
 func GetSerialNumber() (string, error) {
-	cmd := exec.Command("/usr/sbin/dmidecode", "-s", "system-serial-number")
+	cmd := exec.Command("sudo", "/usr/sbin/dmidecode", "-s", "system-serial-number")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
@@ -27,7 +27,7 @@ func GetSerialNumber() (string, error) {
 
 // GetUUID retrieves the UUID of the machine.
 func GetUUID() (string, error) {
-	cmd := exec.Command("/usr/sbin/dmidecode", "-s", "system-uuid")
+	cmd := exec.Command("sudo", "/usr/sbin/dmidecode", "-s", "system-uuid")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
